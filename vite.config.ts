f5f1389044e56437f2ef5061b537fa998aac7724
Sylvain Drexler Wilvins <tsgroup420@gmail.com>
2025-07-08 23:16:51 -0300

    cambiar los puertos

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5137,
+    host: true, // Permite acceso desde fuera del contenedor
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
